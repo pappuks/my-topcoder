57ac669bbf9884d9cb87f8dbfc953103f04f9cb4
Kuldeep Singh <kuldeep.singh@roche.com>
2020-01-08 15:34:49 -0800

    Added file

--- LayeredGlass/src/main/java/Main.java
+++ LayeredGlass/src/main/java/Main.java
@@ -2,13 +2,52 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-	main.minDefects(new String[]{}, new String[]{});
+		System.out.println(main.minDefects(new String[]{"X.",".."}, new String[]{"..",".X"}));
+		System.out.println(main.minDefects(new String[]{"XX..","X...","XX..","X..."}, new String[]{"XXXX","X.X.","....","...."}));
     }
 
     public int minDefects(String[] a, String[] b) {
-     	System.out.println("hello");
-        System.out.println(a.length);
+    	int len = a.length;
+    	int[] defects = new int[8];
 
-        return 0;
+		
+		for (int i = 0; i < len; i++) {
+			for(int j = 0; j < len; j++) {
+				char ca = a[i].charAt(j);
+				if (ca == 'X' || b[i].charAt(j) == 'X') {
+					defects[0]++;
+				}
+				if (ca == 'X' || b[i].charAt(len-1-j) == 'X') {
+					defects[1]++;
+				}
+				if (ca == 'X' || b[len-1-i].charAt(j) == 'X') {
+					defects[2]++;
+				}
+				if (ca == 'X' || b[len-1-i].charAt(len-1-j) == 'X') {
+					defects[3]++;
+				}
+				if (ca == 'X' || b[j].charAt(i) == 'X') {
+					defects[4]++;
+				}
+				if (ca == 'X' || b[j].charAt(len-1-i) == 'X') {
+					defects[5]++;
+				}
+				if (ca == 'X' || b[len-1-j].charAt(i) == 'X') {
+					defects[6]++;
+				}
+				if (ca == 'X' || b[len-1-j].charAt(len-1-i) == 'X') {
+					defects[7]++;
+				}
+			}
+		}
+
+		int min = Integer.MAX_VALUE;
+		for(int i = 0; i < 8; i++) {
+			if (defects[i] < min) {
+				min = defects[i];
+			}
+		}
+
+        return min;
     }
 }
